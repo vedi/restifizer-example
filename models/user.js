5e8b698fd5a215033260e801a1a261e7a5165ba9
igor.basov <igbasov@vedidev.com>
2015-10-02 08:40:43 +0300

    User model redefined.

--- models/user.js
+++ models/user.js
@@ -1,55 +1,81 @@
 /**
  * Created by vedi on 23/08/14.
  */
+
+'use strict';
+
 /**
- * Created by vedi on 11/15/13.
+ * Module dependencies.
  */
+var mongoose = require('../config/mongoose'),
+	Schema = mongoose.Schema,
+	_ = require('lodash'),
+	crypto = require('crypto');
 
-var mongoose    = require('../config/mongoose');
-var crypto      = require('crypto');
+var ROLES = {
+	USER: 'user',
+	STAFF: 'staff',
+	ADMIN: 'admin'
+};
 
-// User
-var UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  hashedPassword: {
-    type: String,
-    required: true
-  },
-  salt: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  scopes: {
-    type: [String],
-    required: true
-  }
+/**
+ * User Schema
+ */
+var UserSchema = new Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	hashedPassword: {
+		type: String,
+		required: true
+	},
+	salt: {
+		type: String,
+		required: true
+	},
+	roles: {
+		type: [{
+			type: String,
+			enum: _.keys(ROLES)
+		}],
+		default: [ROLES.USER]
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
 });
 
-UserSchema.methods.encryptPassword = function(password) {
-  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+/**
+ * Create instance method for hashing a password
+ */
+UserSchema.methods.hashPassword = function (password) {
+	if (this.salt && password) {
+		return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+	} else {
+		return password;
+	}
+};
+
+/**
+ * Create instance method for authenticating user
+ */
+UserSchema.methods.authenticate = function (password) {
+	return this.hashedPassword === this.hashPassword(password);
 };
 
 UserSchema.virtual('password')
-  .set(function(password) {
-    this._plainPassword = password;
-    this.salt = crypto.randomBytes(32).toString('base64');
-    this.hashedPassword = this.encryptPassword(password);
-  })
-  .get(function() {
-    return this._plainPassword;
-  });
+	.set(function(password) {
+		this._plainPassword = password;
+		this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+		this.hashedPassword = this.hashPassword(password);
+	})
+	.get(function() {
+		return this._plainPassword;
+	});
 
-
-UserSchema.methods.checkPassword = function(password) {
-  return this.encryptPassword(password) === this.hashedPassword;
-};
+UserSchema.statics.ROLES = ROLES;
 
 module.exports = mongoose.model('User', UserSchema);
