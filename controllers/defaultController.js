7b1ce5a2017d215a90d37c43f8f937434f1dacaf
igor.basov <igbasov@vedidev.com>
2015-10-02 08:36:36 +0300

    Default restifizer controller have been modified.

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,27 +1,47 @@
 /**
  * Created by vedi on 23/08/14.
  */
+'use strict';
 
 var
   Restifizer = require('restifizer'),
-  passport = require('oauthifizer').passport;
+  passport = require('oauthifizer').passport,
+  User = require('../models/user');
 
-var DefaultController = Restifizer.Controller.extend({
+module.exports = Restifizer.Controller.extend({
+  hasRole: function (req, role) {
+    return req.user &&
+      req.user.roles &&
+      req.user.roles.indexOf(role) >= 0;
+  },
   isAdmin: function (req) {
-    return req.authInfo
-      && req.authInfo.scope
-      && req.authInfo.scope.indexOf("all") >= 0;
+    return this.hasRole(req, User.ROLES.ADMIN);
+  },
+  isStaff: function (req) {
+    return this.hasRole(req, User.ROLES.STAFF);
   },
   actions: {
-    default: {
-      auth: 'bearer' // default auth strategy
+    'default': {
+      enabled: true,
+      auth: ['bearer']
     }
   },
   getAuth: function (options) {
-    return options.auth ? passport.authenticate(options.auth, { session: false }) : function(req, res, callback) {
+    var auths = [
+      passport.authenticate(options.auth, { session: false }),
+      function (req, res, next) {
+        if (!req.isAuthenticated()) {
+          //options
+          return res.status(401).send({
+            message: 'User is not logged in'
+          });
+        }
+
+        next();
+      }
+    ];
+    return options.auth ? auths : function(req, res, callback) {
       callback();
     };
   }
 });
-
-module.exports = DefaultController;
